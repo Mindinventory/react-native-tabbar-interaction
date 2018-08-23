cc2729ba176e73dade56546d2f1c3a9299d99bd0
Paresh Solanki <punja.mindinventory@gmail.com>
2018-08-23 11:18:32 +0530

    Bug fixes and changes in .readme file.

--- README.md
+++ README.md
@@ -26,39 +26,41 @@ iPhone: `react-native run-ios`
 import TabBar from "react-native-tab-bar-interaction";
 ...
   render() {
-    return (<TabBar>
-             <TabBar.Item
-                 icon={require('./tab1.png')}
-                 selectedIcon={require('./tab1_sel.png')}
-                 title="Tab1"
-                 screenBackgroundColor={{ backgroundColor: '#008080' }}
-             >
-               <View>
-   
-               </View>
-             </TabBar.Item>
-             <TabBar.Item
-                 icon={require('./tab2.png')}
-                 selectedIcon={require('./tab2_sel.png')}
-                 title="Tab2"
-                 screenBackgroundColor={{ backgroundColor: '#F08080' }}
-             >
-               <View>
-   
-               </View>
-             </TabBar.Item>
-             <TabBar.Item
-                 icon={require('./tab3.png')}
-                 selectedIcon={require('./tab3_sel.png')}
-                 title="Tab3"
-                 screenBackgroundColor={{ backgroundColor: '#485d72' }}
-             >
-               <View>
-   
-               </View>
-             </TabBar.Item>
-           </TabBar>);
-  }
+      return (
+          <TabBar>
+            <TabBar.Item
+                icon={require('./tab1.png')}
+                selectedIcon={require('./tab1.png')}
+                title="Tab1"
+                screenBackgroundColor={{ backgroundColor: '#008080' }}
+            >
+              <View>
+                  {/*Page Content*/}
+              </View>
+            </TabBar.Item>
+            <TabBar.Item
+                icon={require('./tab2.png')}
+                selectedIcon={require('./tab1.png')}
+                title="Tab2"
+                screenBackgroundColor={{ backgroundColor: '#F08080' }}
+            >
+                <View>
+                    {/*Page Content*/}
+                </View>
+            </TabBar.Item>
+            <TabBar.Item
+                icon={require('./tab3.png')}
+                selectedIcon={require('./tab1.png')}
+                title="Tab3"
+                screenBackgroundColor={{ backgroundColor: '#485d72' }}
+            >
+              <View>
+                  {/*Page Content*/}
+              </View>
+            </TabBar.Item>
+          </TabBar>
+      );
+    }
 ```
 
 ## Component props
--- TabBar.js
+++ TabBar.js
@@ -18,6 +18,9 @@ const AnimatedPath = Animated.createAnimatedComponent(Path)
 
 class TabBarItem extends Component {
 
+    constructor(props) {
+        super(props);
+    }
     render() {
         let child = this.props.children;
 
@@ -37,33 +40,37 @@ export default class TabBar extends Component{
     constructor(props) {
         super(props);
 
-        if (this.props.children.length > 3) {
+        if (this.props.children.length != 3) {
             throw new Error("Three tab should be work.");
         }
         this.state = {
             selectedIndex: 1,
             defaultPage: 1,
+            navFontSize: 12,
+            navTextColor: "rgb(148, 148, 148)",
+            navTextColorSelected: 'rgb(51, 163, 244)',
             circleRadius: new Animated.Value(546),
             pathD: new Animated.Value(357),
             pathX: "357",
             pathY: "675",
             pathA: "689",
             pathB: "706",
+            showIcon: true
         }
 
         this.state.circleRadius.addListener( (circleRadius) => {
             this._myCircle.setNativeProps({ cx: circleRadius.value.toString() });
-        });
+    });
 
         this.state.pathD.addListener( a => {
 
             this.setState({
-                pathX: a.value.toString(),
-                pathY: (318 + parseInt(a.value)).toString(),
-                pathA: (335 + parseInt(a.value)).toString(),
-                pathB: (335 + parseInt(a.value)).toString()
-            })
+            pathX: a.value.toString(),
+            pathY: (318 + parseInt(a.value)).toString(),
+            pathA: (335 + parseInt(a.value)).toString(),
+            pathB: (335 + parseInt(a.value)).toString()
         })
+    })
     }
 
     render() {
@@ -72,73 +79,98 @@ export default class TabBar extends Component{
         } = this.props;
         const {
             selectedIndex,
+            navFontSize,
+            navTextColor,
+            navTextColorSelected,
+            showIcon
         } = this.state;
+
+        let _d;
+        if(this.state.pathY == '' && this.state.pathA == '' && this.state.pathB == ''){
+            _d = `1c-1.1-17.2,12.7-31.7,29.9-31.7h21.3c16.6`
+        } else{
+            _d = `M30,60h${this.state.pathX}.3c17.2,0,31,14.4,30,31.6c-0.2,2.7-0.3,5.5-0.3,8.2c0,71.2,58.1,129.6,129.4,130c72.1,0.3,130.6-58,130.6-130c0-2.7-0.1-5.4-0.2-8.1c-1.1-17.2,12.7-31.7,29.9-31.7h21.3c16.6,0,30,13.4,30,30v94c0,42-34,76-76,76H76c-42,0-76-34-76-76V90C0,73.4,13.4,60,30,60z`
+        }
 
         return(
             <View style={[styles.container,this.props.style, children[this.state.selectedIndex].props.screenBackgroundColor ?  children[this.state.selectedIndex].props.screenBackgroundColor : '#008080']}>
-                {children[selectedIndex]}
-                <View style={[styles.content]}>
-                    <View style={styles.subContent}>
-                        {
-                            React.Children.map(children,  (child,i) => {
-                                const imgSrc = selectedIndex == i
-                                    ?
-                                    <View style={styles.circle}>
-                                        <Image
-                                            style={styles.navImage}
-                                            resizeMode="cover"
-                                            source={child.props.selectedIcon}
-                                        />
-                                    </View>
-                                    :
-                                    <Image
-                                        style={styles.navImage}
-                                        resizeMode="cover"
-                                        source={child.props.icon}
-                                    />;
-                                return (
+        {children[selectedIndex]}
+    <View style={[styles.content]}>
+            <View style={styles.subContent}>
+        {
+            React.Children.map(children,  (child,i) => {
+                const imgSrc = selectedIndex == i && showIcon
+                    ?
+            <View style={styles.circle}>
+        <Image
+            style={styles.navImage}
+            resizeMode="cover"
+            source={child.props.selectedIcon}
+        />
+        </View>
+        :
+        <Image
+            style={styles.navImage}
+            resizeMode="cover"
+            source={child.props.icon}
+        />;
+            return (
 
-                                    <TouchableHighlight
-                                        key={i}
-                                        underlayColor={"transparent"}
-                                        style={styles.navItem}
-                                        onPress={() => this.update(i)}
-                                    >
+                <TouchableHighlight
+            key={i}
+            underlayColor={"transparent"}
+            style={styles.navItem}
+            onPress={() => this.update(i)}
+        >
 
-                                        {imgSrc}
-                                    </TouchableHighlight>
-                                );
-
-                            })
-                        }
-                    </View>
-                    <Svg version="1.1" id="bottom-bar" x="0px" y="0px" width='100%' height="100" viewBox="0 0 1092 260" space="preserve">
-                        <AnimatedPath fill="#f0f0f0" d={ `M30,60h${this.state.pathX}.3c17.2,0,31,14.4,30,31.6c-0.2,2.7-0.3,5.5-0.3,8.2c0,71.2,58.1,129.6,129.4,130c72.1,0.3,130.6-58,130.6-130c0-2.7-0.1-5.4-0.2-8.1C${this.state.pathY}.7,74.5,${this.state.pathA}.5,60,${this.state.pathB}.7,60H1062c16.6,0,30,13.4,30,30v94c0,42-34,76-76,76H76c-42,0-76-34-76-76V90C0,73.4,13.4,60,30,60z` }/>
-                        <AnimatedCircle ref={ ref => this._myCircle = ref } fill="#f0f0f0" cx="546" cy="100" r="100"  />
-                    </Svg>
-                </View>
-            </View>
+            {imgSrc}
+        </TouchableHighlight>
         );
+
+        })
+        }
+    </View>
+        <Svg version="1.1" id="bottom-bar" x="0px" y="0px" width='100%' height="100" viewBox="0 0 1092 260" space="preserve">
+            <AnimatedPath fill="#f0f0f0" d={ `M30,60h${this.state.pathX}.3c17.2,0,31,14.4,30,31.6c-0.2,2.7-0.3,5.5-0.3,8.2c0,71.2,58.1,129.6,129.4,130c72.1,0.3,130.6-58,130.6-130c0-2.7-0.1-5.4-0.2-8.1C${this.state.pathY}.7,74.5,${this.state.pathA}.5,60,${this.state.pathB}.7,60H1062c16.6,0,30,13.4,30,30v94c0,42-34,76-76,76H76c-42,0-76-34-76-76V90C0,73.4,13.4,60,30,60z` }/>
+    <AnimatedCircle ref={ ref => this._myCircle = ref } fill="#f0f0f0" cx="546" cy="100" r="100"  />
+            </Svg>
+            </View>
+            </View>
+    );
     }
     update(index) {
+
+        let that = this
+        that.setState({
+            selectedIndex: index,
+            showIcon: false
+        });
         if(index == 0){
-            this.setState({
-                selectedIndex: index,
-            });
-            Animated.spring( this.state.pathD, { toValue: 22,duration: 10, friction: 10 }).start();
-            Animated.spring( this.state.circleRadius, { toValue: 211, friction: 10 } ).start();
+            Animated.spring( that.state.pathD, { toValue: 22,duration: 10, friction: 10 }).start()
+            setTimeout(function() {
+                that.setState({
+                    showIcon: true
+                })
+            }, 100)
+            Animated.spring( that.state.circleRadius, { toValue: 211, friction: 10 } ).start();
         } else if(index == 2){
-            this.setState({
-                selectedIndex: index,
-            });
-            Animated.spring( this.state.pathD, { toValue: 691,duration: 10, friction: 10 }).start();
-            Animated.spring( this.state.circleRadius, { toValue: 880, friction: 10 } ).start();
+            Animated.spring( that.state.pathD, { toValue: 691,duration: 10, friction: 10 }).start();
+
+            setTimeout(function() {
+                that.setState({
+                    showIcon: true
+                })
+            }, 100)
+            Animated.spring( that.state.circleRadius, { toValue: 880, friction: 10 } ).start()
         } else{
-            this.setState({
-                selectedIndex: index,
-            });
-            Animated.spring( this.state.pathD, { toValue: 357,duration: 10, friction: 10 }).start();
-            Animated.spring( this.state.circleRadius, { toValue: 546, friction: 10 } ).start();
+            Animated.spring( that.state.pathD, { toValue: 357,duration: 10, friction: 10 }).start();
+
+            setTimeout(function() {
+                that.setState({
+                    showIcon: true
+                })
+            }, 100)
+            Animated.spring( that.state.circleRadius, { toValue: 546, friction: 10 } ).start();
         }
     }
 }
@@ -178,7 +210,7 @@ const styles = StyleSheet.create({
     },
     circle: {
         bottom: 18,
-    },
+    }
 });
 
 
