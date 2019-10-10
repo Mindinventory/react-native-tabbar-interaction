672b06c6b6d29ffe40e4264a82a0012969a54758
longnk-1447 <nguyen.khanh.long@sun-asterisk.com>
2019-10-10 10:35:18 +0700

    update

--- TabBar.js
+++ TabBar.js
@@ -74,9 +74,7 @@ export default class TabBar extends Component{
 
     render() {
         const {
-            children, 
-            bgNavBar,
-            bgNavBarSelector,
+            children,
         } = this.props;
         const {
             selectedIndex,
@@ -132,8 +130,8 @@ export default class TabBar extends Component{
         }
     </View>
         <Svg version="1.1" id="bottom-bar" x="0px" y="0px" width='100%' height="100" viewBox="0 0 1092 260" space="preserve">
-            <AnimatedPath fill={bgNavBar ? bgNavBar : '#f0f0f0'} d={ `M30,60h${this.state.pathX}.3c17.2,0,31,14.4,30,31.6c-0.2,2.7-0.3,5.5-0.3,8.2c0,71.2,58.1,129.6,129.4,130c72.1,0.3,130.6-58,130.6-130c0-2.7-0.1-5.4-0.2-8.1C${this.state.pathY}.7,74.5,${this.state.pathA}.5,60,${this.state.pathB}.7,60H1062c16.6,0,30,13.4,30,30v94c0,42-34,76-76,76H76c-42,0-76-34-76-76V90C0,73.4,13.4,60,30,60z` }/>
-    <AnimatedCircle ref={ ref => this._myCircle = ref } fill={bgNavBarSelector ? bgNavBarSelector : '#f0f0f0'} cx="546" cy="100" r="100"  />
+            <AnimatedPath fill="#f0f0f0" d={ `M30,60h${this.state.pathX}.3c17.2,0,31,14.4,30,31.6c-0.2,2.7-0.3,5.5-0.3,8.2c0,71.2,58.1,129.6,129.4,130c72.1,0.3,130.6-58,130.6-130c0-2.7-0.1-5.4-0.2-8.1C${this.state.pathY}.7,74.5,${this.state.pathA}.5,60,${this.state.pathB}.7,60H1062c16.6,0,30,13.4,30,30v94c0,42-34,76-76,76H76c-42,0-76-34-76-76V90C0,73.4,13.4,60,30,60z` }/>
+    <AnimatedCircle ref={ ref => this._myCircle = ref } fill="#f0f0f0" cx="546" cy="100" r="100"  />
             </Svg>
             </View>
             </View>
