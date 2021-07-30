c9c2260d06e34f4e5c89311df6b4741084fe0df7
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2021-07-30 11:27:59 +0530

    fix animation

--- StaticTabbar.js
+++ StaticTabbar.js
@@ -1,102 +1,112 @@
 import * as React from "react";
 import {
-  View, StyleSheet, TouchableWithoutFeedback, Animated, Dimensions, Text, TouchableOpacity
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Animated,
+  Dimensions,
+  Text,
 } from "react-native";
 let { width } = Dimensions.get("window");
 
 export default class StaticTabbar extends React.PureComponent {
-
   constructor(props) {
     super(props);
     const { tabs } = this.props;
-    this.values = tabs.map((tab, index) => new Animated.Value(index === 0 ? 1 : 0));
+    this.values = tabs.map(
+      (tab, index) => new Animated.Value(index === 0 ? 1 : 0)
+    );
   }
 
   onPress = (index) => {
     const { value, tabs } = this.props;
-    const tabWidth = width / tabs.length ;
+    const tabWidth = width / tabs.length;
     Animated.sequence([
       Animated.parallel(
-        this.values.map(v => Animated.timing(v, {
-          toValue: 0,
-          duration: 100,
-          useNativeDriver: true,
-        })),
+        this.values.map((v) =>
+          Animated.timing(v, {
+            toValue: 0,
+            duration: 50,
+            useNativeDriver: true,
+          })
+        )
       ),
-      Animated.parallel([
-        Animated.timing(value, {
-          toValue: tabWidth * index,
-          useNativeDriver: true,
-          bounciness:2
-        }),
-        Animated.timing(this.values[index], {
-          toValue: 1,
-          useNativeDriver: true,
-        }),
-      ]),
+      Animated.timing(value, {
+        toValue: tabWidth * index,
+        useNativeDriver: true,
+        bounciness: 2,
+      }),
+      Animated.timing(this.values[index], {
+        toValue: 1,
+        useNativeDriver: true,
+        duration: 750,
+      }),
     ]).start();
-  }
+  };
 
   render() {
     const { onPress } = this;
-    const { tabs, value, activeTabBackground, labelStyle, onTabChange } = this.props;
-       
-    let mergeLabelStyle = {...styles.labelStyle, ...labelStyle}
-    let newActiveIcon = [styles.activeIcon, {backgroundColor: activeTabBackground ? activeTabBackground : '#ccc'}]
+    const {
+      tabs,
+      value,
+      activeTabBackground,
+      labelStyle,
+      onTabChange,
+      Hvalue,
+    } = this.props;
+
+    let mergeLabelStyle = { ...styles.labelStyle, ...labelStyle };
+    let newActiveIcon = [
+      styles.activeIcon,
+      { backgroundColor: activeTabBackground ? activeTabBackground : "#ccc" },
+    ];
     return (
       <View style={styles.container}>
-        {
-          tabs.map((tab, key) => {
-            const tabWidth = width / tabs.length;
-            const cursor = tabWidth * key;
-            const opacity = value.interpolate({
-              inputRange: [cursor - tabWidth, cursor, cursor + tabWidth],
-              outputRange: [1, 0, 1],
-              extrapolate: "clamp",
-            });
-            const translateY = this.values[key].interpolate({
-              inputRange: [0, 1],
-              outputRange: [64, 0],
-              extrapolate: "clamp",
-            });
-            const opacity1 = this.values[key].interpolate({
-              inputRange: [0, 1],
-              outputRange: [0, 1],
-              extrapolate: "clamp",
-            });
-            return (
-              <React.Fragment {...{ key }}>
-                <TouchableWithoutFeedback onPress={() => {onPress(key);onTabChange && onTabChange() }}>
-                  
-                  <Animated.View style={[styles.tab, { opacity }]}>
-                    {/* <Icon name={tab.name} color="black" size={25} /> */}
-                    {/* <Icon name="home" color="black" size={25} /> */}
-                    {/* <Text>{tab.name}</Text> */}
-                    {tab.inactiveIcon}
-                    <Text style={mergeLabelStyle}>{tab.name}</Text>       
-                  </Animated.View>
-                </TouchableWithoutFeedback>
-                <Animated.View
-                  style={{
-                    position: "absolute",
-                    top: -8,
-                    left: tabWidth * key,
-                    width: tabWidth,
-                    height: 64,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    opacity: opacity1,                    
-                    transform: [{ translateY }],
-                  }}
-                >
-                  <View style={newActiveIcon}>                                        
-                    {tab.activeIcon}          
-                  </View>
+        {tabs.map((tab, key) => {
+          const tabWidth = width / tabs.length;
+          const cursor = tabWidth * key;
+          const opacity = value.interpolate({
+            inputRange: [cursor - tabWidth, cursor, cursor + tabWidth],
+            outputRange: [1, 0, 1],
+            extrapolate: "clamp",
+          });
+
+          const opacity1 = this.values[key].interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 1],
+            extrapolate: "clamp",
+          });
+          return (
+            <React.Fragment {...{ key }}>
+              <TouchableWithoutFeedback
+                onPress={() => {
+                  onPress(key);
+                  onTabChange && onTabChange();
+                }}
+              >
+                <Animated.View style={[styles.tab, { opacity, zIndex: 100 }]}>
+                  {tab.inactiveIcon}
+                  <Text style={mergeLabelStyle}>{tab.name}</Text>
                 </Animated.View>
-              </React.Fragment>
-            );
-          })
-        }
+              </TouchableWithoutFeedback>
+              <Animated.View
+                style={{
+                  position: "absolute",
+                  top: -8,
+                  left: tabWidth * key,
+                  width: tabWidth,
+                  height: 64,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  opacity: opacity1,
+                  zIndex: 50,
+                }}
+              >
+                <View style={newActiveIcon}>{tab.activeIcon}</View>
+              </Animated.View>
+            </React.Fragment>
+          );
+        })}
       </View>
     );
   }
@@ -104,30 +114,26 @@ export default class StaticTabbar extends React.PureComponent {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",     
-    
+    flexDirection: "row",
   },
   tab: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     height: 64,
-
   },
   activeIcon: {
-    // backgroundColor: "#ccc",
     width: 60,
     height: 60,
     borderRadius: 50,
     marginBottom: 30,
     justifyContent: "center",
     alignItems: "center",
-    
   },
   labelStyle: {
     fontSize: 11,
-    fontWeight: '600',
+    fontWeight: "600",
     marginTop: 3,
-    color: '#000'    
-  }
+    color: "#000",
+  },
 });
--- TabBar.js
+++ TabBar.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,106 +6,118 @@ import {
   View,
   Animated,
   Text,
-} from 'react-native';
-import * as shape from 'd3-shape';
-import Svg, { Circle, Path } from 'react-native-svg';
-import StaticTabbar from './StaticTabbar';
-
+} from "react-native";
+import * as shape from "d3-shape";
+import Svg, { Circle, Path } from "react-native-svg";
+import StaticTabbar from "./StaticTabbar";
 
 const AnimatedSvg = Animated.createAnimatedComponent(Svg);
-let { width } = Dimensions.get('window');
+let { width } = Dimensions.get("window");
 const height = 60;
-// const { Path } = Svg;
 
 const getPath = (tabWidth) => {
-
   const left = shape
     .line()
-    .x(d => d.x)
-    .y(d => d.y)([
-      { x: 0, y: 0 },
-      { x: width + tabWidth / 2, y: 0 },
-    ]);
+    .x((d) => d.x)
+    .y((d) => d.y)([
+    { x: 0, y: 0 },
+    { x: width + tabWidth / 2, y: 0 },
+  ]);
   const tab = shape
     .line()
-    .x(d => d.x)
-    .y(d => d.y)
+    .x((d) => d.x)
+    .y((d) => d.y)
     .curve(shape.curveBasis)([
-      { x: width + tabWidth / 2 - 100, y: 0 },
-      { x: width + tabWidth / 2 - 65 + (-35), y: 0 },
-      { x: width + tabWidth / 2 - 50 + 10, y: -6 },
-      { x: width + tabWidth / 2 - 50 + 15, y: height - 9 },
-      { x: width + tabWidth / 2 + 50 - 15, y: height - 9 },
-      { x: width + tabWidth / 2 + 50 - 10, y: -6 },
-      { x: width + tabWidth / 2 + 65 - (-35), y: 0 },
-      { x: width + tabWidth / 2 + 100, y: 0 },
-    ]);
+    { x: width + tabWidth / 2 - 100, y: 0 },
+    { x: width + tabWidth / 2 - 65 + -35, y: 0 },
+    { x: width + tabWidth / 2 - 50 + 10, y: -6 },
+    { x: width + tabWidth / 2 - 50 + 15, y: height - 9 },
+    { x: width + tabWidth / 2 + 50 - 15, y: height - 9 },
+    { x: width + tabWidth / 2 + 50 - 10, y: -6 },
+    { x: width + tabWidth / 2 + 65 - -35, y: 0 },
+    { x: width + tabWidth / 2 + 100, y: 0 },
+  ]);
   const right = shape
     .line()
-    .x(d => d.x)
-    .y(d => d.y)([
-      { x: width, y: 0 },
-      { x: width * 2, y: 0 },
-      { x: width * 2, y: height },
-      { x: 0, y: height },
-      { x: 0, y: 0 },
-    ]);
+    .x((d) => d.x)
+    .y((d) => d.y)([
+    { x: width, y: 0 },
+    { x: width * 2, y: 0 },
+    { x: width * 2, y: height },
+    { x: 0, y: height },
+    { x: 0, y: 0 },
+  ]);
   return `${left} ${tab} ${right}`;
 };
 
-export default class TabBar extends React.PureComponent {
+export default class Tabbar extends React.PureComponent {
   value = new Animated.Value(0);
 
   render() {
-    const { tabs, tabBarBackground, tabBarContainerBackground } = this.props
+    const { tabs, tabBarBackground, tabBarContainerBackground } = this.props;
     const { value } = this;
     const translateX = value.interpolate({
       inputRange: [0, width],
       outputRange: [-width, 0],
     });
-    let tabBarBackgroundColor = tabBarBackground ?  tabBarBackground : '#ccc'
+    let tabBarBackgroundColor = tabBarBackground ? tabBarBackground : "#ccc";
     const tabWidth =
-      tabs.length > 0 ? width / tabs.length : console.error('please add tab data');
+      tabs.length > 0
+        ? width / tabs.length
+        : console.error("please add tab data");
     const d = getPath(tabWidth);
-    
+
     if (tabs.length > 0) {
       return (
         <>
           <View
-            style={{               
-              backgroundColor: tabBarContainerBackground ? tabBarContainerBackground : '#fff',    
-              position: 'absolute',
-              bottom: 0    ,
-            
+            style={{
+              backgroundColor: tabBarContainerBackground
+                ? tabBarContainerBackground
+                : "#fff",
+              position: "absolute",
+              bottom: 0,
             }}
           >
             <View
               {...{
                 height,
-                width,            
-              }}>
+                width,
+              }}
+            >
               <AnimatedSvg
                 width={width * 2}
                 {...{ height }}
                 style={{
                   transform: [{ translateX }],
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
                 <Path fill={tabBarBackgroundColor} {...{ d }} />
               </AnimatedSvg>
               <View style={StyleSheet.absoluteFill}>
-                <StaticTabbar {...this.props} {...{ tabs, value }} />
+                <StaticTabbar
+                  {...this.props}
+                  Hvalue={translateX}
+                  {...{ tabs, value }}
+                />
               </View>
             </View>
-            <SafeAreaView style={[styles.container, { backgroundColor: tabBarBackgroundColor }]} />
+            <SafeAreaView
+              style={[
+                styles.container,
+                { backgroundColor: tabBarBackgroundColor },
+              ]}
+            />
           </View>
-         
         </>
       );
     } else {
       return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
           <Text>Please add tab data</Text>
         </View>
       );
@@ -115,6 +127,6 @@ export default class TabBar extends React.PureComponent {
 
 const styles = StyleSheet.create({
   container: {
-    width: Dimensions.get('window').width,
+    width: Dimensions.get("window").width,
   },
 });
